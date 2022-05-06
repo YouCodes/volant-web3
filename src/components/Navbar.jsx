1cdf7cc353d05ef7c7684df355fe3ca01eba2c06
Louis You <81671608+YouCodes@users.noreply.github.com>
2022-05-06 06:53:44 -0700

    Complete Penultimate Components for Landing Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-48 cursor-pointer" />
+        <img src={logo} alt="logo" className="w-48 cursor-pointer" href="/" />
       </div>
       <ul className="text-black md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,100 @@
 import React from 'react'
+import { useContext } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+import { Loader } from ".";
 
 function Welcome() {
+  const connectWallet = () => {
+
+  }
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex text-sm font-bold text-black";
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-1.5 outline-none bg-transparent text-black border-none text-sm white-glassmorphism"
+  />
+);
+
+
   return (
-    <div>Welcome</div>
+    <div className="flex w-full justify-center items-center">
+    <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex flex-1 justify-start flex-col md:mr-10 md:mt-5">
+        <h1 className="text-left  text-5xl text-black text-gradient py-1">
+          Send Crypto <br /> around the world
+        </h1>
+        <p className="text-left mt-5 text-black font-light md:w-9/12 w-11/12 text-base"> Volant Allows You to Buy and Sell <br /> Crypto Easily and at Lightning Speed </p>
+        <button type="button" onClick={connectWallet} className="flex mt-14 justify-center items-center my-5 bg-[#2952e3] p-3 mr-10 rounded-full cursor-pointer hover:bg-[#2546bd]"> 
+       <p className="text-white text-base font-semibold"> Connect Metamask</p>
+        </button> 
+    <div className="py-2 grid sm:grid-cols-3 grid-cols-2 w-full mt-10"> 
+
+      <div className={commonStyles}>
+        Reliable
+      </div>
+        <div className={commonStyles}>
+        Fast
+      </div>
+        <div className={commonStyles}>
+        Accessible
+      </div>
+        <div className={commonStyles}>
+        Low Fees
+      </div>
+        <div className={commonStyles}>
+        Stable
+      </div>
+        <div className={commonStyles}>
+        Immutable
+      </div>
+    </div> 
+      </div>
+
+      <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+      <div className="flex justify-between flex-col w-full h-full">
+        <div className="flex justify-between items-start">
+          <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+            <SiEthereum fontsize={21} color="#fff" />
+          </div>
+          <BsInfoCircle fontSize={17} color="#fff" />
+        </div>
+         <p className="mt-10 text-left text-white font-light text-sm">
+               Address {/* {shortenAddress(currentAccount)} */}
+        </p>
+        <p className="text-left text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+
+      </div>
+    </div>    
+ <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+     <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}} />
+     <Input placeholder="Amount (Eth)" name="amount" type="text" handleChange={()=>{}} />
+     <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}} />
+     <Input placeholder="Message" name="message" type="text" handleChange={()=>{}} />
+<div className="h-[1px] w-full bg-gray-400 my-2" />
+    
+          
+            
+                <button
+                  type="button"
+                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] bg-[#2952e3] hover:bg-[#2546bd] rounded-full cursor-pointer"
+                >
+                  Send Now
+                </button>
+        
+     </div>
+</div> 
+
+    </div>
+    </div>
   )
 }
 
